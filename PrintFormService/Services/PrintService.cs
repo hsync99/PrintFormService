8a0dfca755c3784f8e82d44cb2c0282c2b2d9d74
Leonid Gavrilov <hsync99@gmail.com>
2023-08-14 23:55:58 +0600

    step

--- PrintFormService/Services/PrintService.cs
+++ PrintFormService/Services/PrintService.cs
@@ -15,19 +15,31 @@ namespace PrintFormService.Services
             String inputfilename = filepath;
            Document document= new Document();
             document.LoadFromFile(inputfilename);
+      
             Section section = document.Sections[0];
             var jdata = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
             int i = 0;
+            int j = 0;
             foreach (var item in jdata)
             {
-                
+
+
                 Paragraph para1 = section.Paragraphs[i];
                 if (para1.Text.Contains(item.Key))
                 {
                     string text = para1.Text;
-                    text = text.Replace(item.Key, item.Value); 
+                    text = text.Replace(item.Key, item.Value);
                     para1.Text = text;
                 }
+                //else
+                //{
+                //    for (j = i; para1.Text.Contains(item.Key); j++)
+                //    {
+                //        para1= section.Paragraphs[j];
+                //    }
+                //}
+
+
                 i++;
             }
            
@@ -37,9 +49,9 @@ namespace PrintFormService.Services
             //Paragraph para2 = section.Paragraphs[1];
             //TextRange tr = para2.AppendText("Spire.Doc for .NET is stand-alone"
             //+ "to enables developers to operate Word witout Microsoft Word installed.");
-            //tr.CharacterFormat.FontName = "Cataneo BT";
+            //tr.CharacterFormat.FontName = "TimesNewRoman";
             //tr.CharacterFormat.FontSize = 12;
-            //tr.CharacterFormat.TextColor = Color.YellowGreen;
+            //tr.CharacterFormat.TextColor = Color.Black;
 
             
             //Save and Launch
